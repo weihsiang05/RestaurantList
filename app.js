d3e03cb9e0aa74758956394481cec72f2a31747a
weihsiang05 <weihsiang0000@gmail.ocm>
2023-08-25 03:12:22 -0400

    Build the route and controller setup

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('express app for restaurant')
+  res.redirect('/restaurant')
+})
+
+app.get('/restaurant', (req, res) => {
+  res.send('list restaurant')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  //req.params.id拿來取得你輸入的id，ex: http://localhost:3000/restaurant/5 -> 就會顯示5
+  const id = req.params.id
+  res.send(`read restaurant: ${id}`)
 })
 
 app.listen(port, () => {
