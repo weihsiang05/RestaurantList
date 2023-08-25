bc1f75214a93acd47b3b0317f7468cf842c006cb
weihsiang05 <weihsiang0000@gmail.ocm>
2023-08-25 19:16:22 -0400

    Add details page

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ app.get('/restaurant', (req, res) => {
 app.get('/restaurant/:id', (req, res) => {
   //req.params.id拿來取得你輸入的id，ex: http://localhost:3000/restaurant/5 -> 就會顯示5
   const id = req.params.id
-  res.send(`read restaurant: ${id}`)
+  //res.send(`read restaurant: ${id}`)
+  const rest = restaurant.find((r) => {
+    return r.id.toString() === id
+  })
+
+  res.render('details', { rest })
 })
 
 app.listen(port, () => {
