35c2681818468ae948a1713a63a372ec6ac8d6d0
weihsiang05 <weihsiang0000@gmail.ocm>
2023-09-26 16:46:36 -0400

    Install and initialize sequelize. Also, modify restaurant migration file.

--- app.js
+++ app.js
@@ -2,45 +2,57 @@ const express = require('express')
 const app = express()
 const { engine } = require('express-handlebars')
 const port = 3000
-const restaurant = require('./public/jasons/restaurant.json').results
+//const restaurant = require('./public/jsons/restaurant.json').results
+const db = require('./models')
+const Restaurant = db.Restaurant
 
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
 // 傳入靜態網頁
-app.use(express.static('public'))
+//app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.redirect('/restaurant')
 })
 
 app.get('/restaurant', (req, res) => {
-  //keyword為<input></input>中的name="keyword"
-  const keywords = req.query.keyword?.trim()
-  const matchedSerach = keywords ? restaurant.filter((rest) => {
-    return Object.values(rest).some((property) => {
-      if (typeof property === 'string') {
-        return property.toLowerCase().includes(keywords.toLowerCase())
-      } else {
-        return false
-      }
+  return Restaurant.findAll()
+    .then((restaurants) => {
+      res.send({ restaurants })
     })
-  }) : restaurant
-
-  res.render('index', { restaurant: matchedSerach, keywords })
+    .catch((err) => {
+      res.status(422).json(err)
+    })
 })
 
-app.get('/restaurant/:id', (req, res) => {
-  //req.params.id拿來取得你輸入的id，ex: http://localhost:3000/restaurant/5 -> 就會顯示5
-  const id = req.params.id
-  //res.send(`read restaurant: ${id}`)
-  const rest = restaurant.find((r) => {
-    return r.id.toString() === id
-  })
+// app.get('/restaurant', (req, res) => {
+//   //keyword為<input></input>中的name="keyword"
+//   const keywords = req.query.keyword?.trim()
+//   const matchedSerach = keywords ? restaurant.filter((rest) => {
+//     return Object.values(rest).some((property) => {
+//       if (typeof property === 'string') {
+//         return property.toLowerCase().includes(keywords.toLowerCase())
+//       } else {
+//         return false
+//       }
+//     })
+//   }) : restaurant
 
-  res.render('details', { rest })
-})
+//   res.render('index', { restaurant: matchedSerach, keywords })
+// })
+
+// app.get('/restaurant/:id', (req, res) => {
+//   //req.params.id拿來取得你輸入的id，ex: http://localhost:3000/restaurant/5 -> 就會顯示5
+//   const id = req.params.id
+//   //res.send(`read restaurant: ${id}`)
+//   const rest = restaurant.find((r) => {
+//     return r.id.toString() === id
+//   })
+
+//   res.render('details', { rest })
+// })
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
---
+++ migrations/20230926201013-create-restaurant.js
@@ -0,0 +1,53 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Restaurants', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      name_en: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      category: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      location: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      google_map: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      rating: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Restaurants');
+  }
+};
